3e37b2147adb346e3e271fa2f249ac31b0f09cd6
Pedroriq <90358361+Pedroriq@users.noreply.github.com>
2022-11-13 17:10:01 -0300

    v1

---
+++ noise_removal.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+# lendo a imagem
+img = cv2.imread('image_1.jpg')
+
+# função da biblioteca opencv para remover os ruidos
+denoising = cv2.fastNlMeansDenoisingColored(img,None,10,10,7,21)
+
+plt.subplot(121),plt.imshow(img)
+plt.subplot(122),plt.imshow(denoising)
+plt.show()
---
+++ noise_removal_video.py
@@ -0,0 +1,39 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+mean = 0
+var = 10
+sigma = var ** 1
+
+# carrregando video escolhido para a variavel cap
+cap = cv2.VideoCapture('Marvel’s Spider-Man – Be Greater Extended Trailer _ PS4.mp4')
+
+# pegando o primeiro frame do vídeo
+img = [cap.read()[1] for i in range(0,1)]
+cv2.imwrite("frames/frame0.png", img[0])
+
+# aplicando o ruído no frame, incrimentando o sigma em cada repetição (5x)
+for i in range(0,5):
+    img2 = cv2.imread("frames/frame{}.png".format(0))
+    gaussian = np.random.normal(mean, sigma + i, (720,1280,3)) #  np.zeros((224, 224), np.float32)
+    noisy_image = img2 + gaussian
+    cv2.imwrite("frames_noise/frame{}_noise.png".format(i), noisy_image)
+
+# abrindo as 5 imagens, o mesmo frame com 5 intensidades de ruídos diferentes
+frame_0 = np.array(Image.open('frames_noise/frame0_noise.png'))
+frame_1 = np.array(Image.open('frames_noise/frame1_noise.png'))
+frame_2 = np.array(Image.open('frames_noise/frame2_noise.png'))
+frame_3 = np.array(Image.open('frames_noise/frame3_noise.png'))
+frame_4 = np.array(Image.open('frames_noise/frame4_noise.png'))
+# aplicando a média aritimética nas 5 imagens
+frame_avg = ( frame_0.astype(np.float64)
+            + frame_1.astype(np.float64)
+            + frame_2.astype(np.float64)
+            + frame_3.astype(np.float64)
+            + frame_4.astype(np.float64)) / 5
+frame_avg = frame_avg.astype(np.uint8)
+
+# salvando a imagem
+Image.fromarray(frame_avg).save('result/noise_removal.png')
